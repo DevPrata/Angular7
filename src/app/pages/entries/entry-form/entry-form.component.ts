a809a054e0f4c669341e0cbd797c3a485b15e505
Guilherme <guilherme.costa@fcamara.com.br>
2020-04-15 14:11:23 -0300

    Terminando componente de entry form

--- src/app/pages/entries/entry-form/entry-form.component.ts
+++ src/app/pages/entries/entry-form/entry-form.component.ts
@@ -4,6 +4,9 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 import { EntryService } from '../shared/entry.service'
 import { Entry } from '../shared/entry.model'
+import { Category } from '../../categories/shared/category.model'
+import { CategoryService } from '../../categories/shared/category.service'
+
 import { switchMap } from 'rxjs/operators'
 
 import toastr from 'toastr'
@@ -21,18 +24,44 @@ export class EntryFormComponent implements OnInit,AfterContentChecked {
   serverErrorMessages: string[] = [];
   submittingForm: boolean = false;
   entry: Entry = new Entry();
+  categories: Array<Category>;
+
+  imaskConfig = {
+    mask:Number,
+    scale:2,
+    thousandsSeparator:'',
+    padFractionalZeros:true,
+    normalizeZeros:true,
+    radix:','
+  }
+
+  ptBR = {
+    firstDayOfWeek: 0,
+    dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+    dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
+    dayNamesMin: ['Do', 'Se', 'Te', 'Qu', 'Qu', 'Se', 'Sa'],
+    monthNames: [
+      'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho',
+      'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+    ],
+    monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+    today: 'Hoje',
+    clear: 'Limpar'
+  }
 
   constructor(
     private entryService:EntryService,
     private route: ActivatedRoute,
     private router: Router,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private categorieService:CategoryService
     ) { }
 
   ngOnInit(): void {
     this.setCurrentAction()
     this.buildEntryForm()
     this.loadEntry()
+    this.loadCategories()
   }
 
   ngAfterContentChecked() {
@@ -46,6 +75,17 @@ export class EntryFormComponent implements OnInit,AfterContentChecked {
       this.createEntry()
     else
       this.updateEntry()
+  }
+
+  get typeOptions(): Array<any> {
+    return Object.entries(Entry.types).map(
+      ([value,text]) =>{
+        return {
+          text: text,
+          value:value
+        }
+      }
+    )
   }
 
 
@@ -64,10 +104,10 @@ export class EntryFormComponent implements OnInit,AfterContentChecked {
       id:[null],
       name:[null, [Validators.required, Validators.minLength(2)]],
       description:[null],
-      type:[null,[Validators.required]],
+      type:['expense',[Validators.required]],
       amount:[null,[Validators.required]],
       date:[null,[Validators.required]],
-      paid:[null,[Validators.required]],
+      paid:[true,[Validators.required]],
       categoryId:[null,[Validators.required]],
     })
   }
@@ -86,6 +126,12 @@ export class EntryFormComponent implements OnInit,AfterContentChecked {
         }
       )
     }
+  }
+
+  private loadCategories(){
+    this.categorieService.getAll().subscribe(
+      categories => this.categories = categories
+    )
   }
 
   private setPageTitle(){
