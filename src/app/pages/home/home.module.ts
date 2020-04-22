34595af2488aa228da3d7b6a4994dfdeb3c17126
Guilherme <guilherme.costa@fcamara.com.br>
2020-04-22 16:03:38 -0300

    Estilo do botão

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { ReportsComponent } from './reports/reports.component';
+import { MatButtonModule } from "@angular/material/button";
 
 
 @NgModule({
   declarations: [ReportsComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatButtonModule
   ]
 })
 export class HomeModule { }
